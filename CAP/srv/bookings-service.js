077f394a4b3a36cda9dbd3972a10055a9cd44c53
i343516 <darius.birescu@sap.com>
2022-06-07 15:50:11 +0300

    Fixed db schema

--- CAP/srv/bookings-service.js
+++ CAP/srv/bookings-service.js
@@ -2,11 +2,14 @@ const cds = require("@sap/cds");
 
 module.exports = cds.service.impl((srv) => {
   srv.before("CREATE", "Bookings", async (req) => {
-    const matchingEmail = String(req.data.emailAddress)
-      .toLowerCase()
-      .match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
-    if (!matchingEmail) {
-      throw new Error("Invalid email!");
+    if (!validateEmail(req.data.emailAddress)) {
+      req.reject("Invalid email address");
     }
   });
 });
+
+function validateEmail(email) {
+  return String(email)
+    .toLowerCase()
+    .match(/^[\w-.]+@([\w-]+.)+[\w-]{2,4}$/);
+}
